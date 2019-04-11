818efd1848c994cf678f56d911789b66a90f980c
Dante Virgilio Manrique Orellano <38734249+DanteManriqueOrellano@users.noreply.github.com>
2019-04-10 23:36:17 -0500

    asd
    das

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-const { ApolloServer} = require('apollo-server');
-const mongoose = require('mongoose')
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 //requisitos para graphql
-const typeDefs = require('./grapqhql/schema')
-const resolvers = require('./grapqhql/resolvers')
+const typeDefs = require("./grapqhql/schema");
+const resolvers = require("./grapqhql/resolvers");
 //requisitos para resolvers
 /**
  * clases API
  * modelos mongoose
- * 
+ *
  */
-const AutorAPI = require('./datasources/autorAPI')
+const AutorAPI = require("./datasources/autorAPI");
 //importa la funcion createModelAutor
-const {createModelAutor} = require('./models/autor')
+const { createModelAutor } = require("./models/autor");
 /**
  * configuracion de la conexion mediante mongooose
  */
-mongoose.connect('mongodb://memoria:a123456789@ds149875.mlab.com:49875/heroku_5jb5hffp',{useNewUrlParser: true})
-  .then(() => console.log('connected to db'))
+mongoose
+  .connect(
+    "mongodb://memoria:a123456789@ds149875.mlab.com:49875/heroku_5jb5hffp",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("connected to db"))
   .catch(err => console.log(err));
 //asigna la funcion que representa el modelo a autor
 const autor = createModelAutor();
@@ -25,15 +29,19 @@ const autor = createModelAutor();
  * Incluir entre paréntesis el cuerpo para retornar un objeto literal:
 (params) => ({foo: bar})
  */
-const dataSources = ()=>({
-    autorapi: new AutorAPI({autor})
-})
-const server = new ApolloServer({ 
-    typeDefs,
-    resolvers,
-    dataSources,
+const dataSources = () => ({
+  autorapi: new AutorAPI({ autor })
 });
-if (process.env.NODE_ENV !== 'test')
-  server
-    .listen({ port: 4000 })
-    .then(({ url }) => console.log(`🚀 app running at ${url}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources,
+  introspection: true,
+  playground: {
+    endpoint: `https://operacionesanidadas.herokuapp.com`
+  }
+});
+if (process.env.NODE_ENV !== "test")
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
